da19e70b7bbb874a4fe85a7a74a31ed55b45c924
Vivek205 <ichbinvivek@gmail.com>
2021-09-21 12:42:17 +0530

    mui theme and emotion

---
+++ snet-ui/Theme/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0052cc",
+    },
+    secondary: {
+      main: "#edf2ff",
+    },
+  },
+});
+
+export default theme;
