ec8023d9bf62e79e07e9549938da42f9a8c89e4c
Vivek205 <ichbinvivek@gmail.com>
2021-09-21 15:20:37 +0530

    storybook

--- snet-ui/Button/PrimaryButton.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@mui/material";
-import { styled } from "@mui/system";
-import React from "react";
-
-const PrimaryButton = styled(Button)`
-  background-color: ${({ theme }) => theme.palette.primary.main};
-`;
-
-export default PrimaryButton;
