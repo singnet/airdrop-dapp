2bce67aac115b2975be54ab955607dd690bf35f2
Rajeev <scraptorajeev@gmail.com>
2021-10-04 15:10:28 +0530

    Fetch Airdrop schedule

---
+++ components/AirdropSchedule/AirdropSchedules.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import AirdropSchedule from "snet-ui/AirdropSchedule/AirdropSchedule";
+import AirdropScheduleTitle from "./AirdropScheduleTitle";
+import axios from "utils/Axios";
+
+const AirdropSchedules = () => {
+  const [schedules, setSchedule] = useState([]);
+
+  useEffect(() => {
+    getAirdropSchedule();
+  }, []);
+
+  const getAirdropSchedule = async () => {
+    try {
+      const payload = {
+        limit: "200",
+        skip: "0",
+      };
+
+      const { data } = await axios.post("/airdrop-schedule", payload);
+      setSchedule(data.data.schedule);
+    } catch (e) {
+      // TODO: Implement error handling
+    }
+  };
+
+  return (
+    <>
+      <AirdropScheduleTitle />
+      {schedules.map((schedule, index) => {
+        return (
+          <AirdropSchedule
+            date={schedule.airdrop_schedule_date}
+            title={schedule.airdrop_schedule_info}
+            description={schedule.airdrop_schedule_description}
+          />
+        );
+      })}
+    </>
+  );
+};
+
+export default AirdropSchedules;
