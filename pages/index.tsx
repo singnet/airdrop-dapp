b9cf63cb99dd173869dd23906a0c2c8e664c9d34
Susmithashanmugam <87366584+Susmithageek1@users.noreply.github.com>
2021-10-12 12:15:15 +0530

    Updated How it works

--- pages/index.tsx
+++ pages/index.tsx
@@ -79,6 +79,11 @@ const HowItWorksSampleData = [
     description:
       "ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, c",
   },
+  {
+    title: "atise Ipsum is simply dummy text of the printing an",
+    description:
+      "there are many variations in the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised",
+  },
 ];
 
 const ScheduleSampleData = [
