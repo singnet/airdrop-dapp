5f3b6b930e8dae49b9913996eac707dfe9e4afeb
Vivek205 <ichbinvivek@gmail.com>
2021-10-18 12:59:15 +0530

    Contact us page

---
+++ pages/contactus.tsx
@@ -0,0 +1,12 @@
+import CommonLayout from "layout/CommonLayout";
+import React from "react";
+
+export default function contactus() {
+  return <CommonLayout>
+        
+      
+      Contact Us
+      
+      
+      </CommonLayout>;
+}
