623ae4c70fb495806b7619c4305ccdff3febac09
Susmithashanmugam <87366584+Susmithageek1@users.noreply.github.com>
2021-10-28 13:38:22 +0530

    Removed unused imports

--- layout/CommonLayout.tsx
+++ layout/CommonLayout.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from "react";
 import { useActiveWeb3React } from "snet-ui/Blockchain/web3Hooks";
 import Falsemessage from "snet-ui/Flasemessage";
-import ListSubheader from "@mui/material/ListSubheader";
+
 import Footer from "snet-ui/Footer";
 import Header from "snet-ui/Header";
 import { setShowConnectionModal } from "utils/store/features/walletSlice";
