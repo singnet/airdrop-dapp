6cc3b6a6bdc6ccdc70ffff7f194bc254b20e7187
Susmithashanmugam <87366584+Susmithageek1@users.noreply.github.com>
2021-10-29 14:57:47 +0530

    Modified FAQ

--- snet-ui/FAQ/index.tsx
+++ snet-ui/FAQ/index.tsx
@@ -1,40 +1,31 @@
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import Typography from "@mui/material/Typography";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddSharpIcon from "@mui/icons-material/AddSharp";
-import { styled } from "@mui/material/styles";
-import { Grid } from "@mui/material";
-import { Box } from "@mui/system";
-import Paper from "@mui/material/Paper";
 import { Button } from "@mui/material";
 //import { faqSampleData } from "sample-data/faq";
 import { faqSampleData } from "./faq";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import MuiAccordion, { AccordionProps } from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import { Box } from "@mui/system";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
 
-const AccordionContainer = styled(Paper)`
-  margin: 20px 0px 0 0px;
-`;
-
-// const Accordion = styled(MUIAccordion)`
-//   flex: none;
-//   transition: fill 0.25s;
-//   width: 48px;
-//   height: 48px;
-
-//   &:hover {
-//     fill: rebeccapurple;
-//   }
-// `;
+const Accordion = styled((props: AccordionProps) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))({
+  borderTop: 0,
+  borderRight: 0,
+  borderLeft: 0,
+});
 
 export default function FAQPage() {
   return (
-    <Box>
-      <Typography align="center" color="bgtext.main" variant="h4">
-        Frequently Asked Questions
-      </Typography>
-      <Box sx={{ mx: [0, 4, 15] }}>
-        <AccordionContainer>
+    <div>
+      <Box>
+        <Typography align="center" variant="h2" color="bgtext.main" mb={5}>
+          Frequently Asked Questions
+        </Typography>
+        <Box sx={{ mx: [0, 7, 20], mt: 3 }}>
           {faqSampleData.map((faq) => (
             <Accordion key={faq.question}>
               <AccordionSummary
@@ -48,7 +39,8 @@ export default function FAQPage() {
                 }}
               >
                 <Typography
-                  color="textAdvanced.main"
+                  color="bgtext.main"
+                  variant="h5"
                   sx={{
                     "&:hover": {
                       color: "secondary.main",
@@ -63,18 +55,25 @@ export default function FAQPage() {
               </AccordionDetails>
             </Accordion>
           ))}
-        </AccordionContainer>
-      </Box>
-      <Box sx={{ p: 3 }}>
-        <Typography align="center" color="textAdvanced.main">
-          Still Have Questions?
-        </Typography>
-        <Box textAlign="center">
-          <Button variant="outlined" size="large" sx={{ justifyContent: "center" }}>
-            <Typography color="secondary.main">Contact us</Typography>
-          </Button>
+        </Box>
+        <Box sx={{ p: 3 }}>
+          <Typography align="center" color="textAdvanced.main" variant="body1">
+            Still Have Questions?
+          </Typography>
+          <Box textAlign="center" sx={{ mt: 1 }}>
+            <Button
+              variant="outlined"
+              size="large"
+              color="secondary"
+              href="/contactus"
+            >
+              <Typography color="secondary.main" variant="subtitle2">
+                Contact us
+              </Typography>
+            </Button>
+          </Box>
         </Box>
       </Box>
-    </Box>
+    </div>
   );
 }
