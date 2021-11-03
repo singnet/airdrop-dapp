adce2b4cac978953761698de81a1bbeb1b5d204a
Susmithashanmugam <87366584+Susmithageek1@users.noreply.github.com>
2021-11-03 12:23:43 +0530

    added stories for FAQ and Falsemesssage

---
+++ snet-ui/FAQ/index.stories.tsx
@@ -0,0 +1,10 @@
+import { Meta } from "@storybook/react";
+import FAQPageComponent from "./";
+
+export default {
+  title: "Components/FAQPage",
+} as Meta;
+
+export const FAQPage: React.VFC<{}> = () => {
+  return <FAQPageComponent />;
+};
