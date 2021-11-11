b22dd0dad6854cd3f99c2b240fe2fc014945c8f8
Vivek205 <ichbinvivek@gmail.com>
2021-11-11 18:17:15 +0530

    local timezone

--- components/Registration/index.tsx
+++ components/Registration/index.tsx
@@ -34,7 +34,7 @@ const DateFormatter = new Intl.DateTimeFormat("en-GB", {
   year: "numeric",
   hour: "numeric",
   minute: "numeric",
-  timeZone: "UTC",
+  // timeZone: "UTC",
   timeZoneName: "short",
 });
 
