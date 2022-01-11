34acf311dbf688b8cd19700f28350445cda61999
Raam <raam@singularitynet.io>
2022-01-11 12:34:25 +0530

    Fixed Metamask wallet not loading on Firefox issue

--- snet-ui/Blockchain/WalletModal.tsx
+++ snet-ui/Blockchain/WalletModal.tsx
@@ -1,36 +1,29 @@
-import * as React from "react";
-import { SxProps, Theme } from "@mui/system";
-import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-// import List from "@mui/material/List";
-// import Avatar from "@mui/material/Avatar";
-// import ListItem from "@mui/material/ListItem";
-// import ListItemAvatar from "@mui/material/ListItemAvatar";
-// import ListItemText from "@mui/material/ListItemText";
-// import PersonIcon from "@mui/icons-material/Person";
-import { SUPPORTED_WALLETS } from "./Wallet";
-import { AbstractConnector } from "@web3-react/abstract-connector";
-import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
-import { UnsupportedChainIdError, useWeb3React } from "@web3-react/core";
-import { useEagerConnect } from "./web3Hooks";
-import Typography from "@mui/material/Typography";
-import DialogContent from "@mui/material/DialogContent";
-import IconButton from "@mui/material/IconButton";
-import CloseIcon from "@mui/icons-material/Close";
-import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
-import Box from "@mui/material/Box";
-import ButtonBase from "@mui/material/ButtonBase";
-// import Image from "next/image";
+import * as React from 'react';
+import { SxProps, Theme } from '@mui/system';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import { SUPPORTED_WALLETS } from './Wallet';
+import { AbstractConnector } from '@web3-react/abstract-connector';
+import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
+import { UnsupportedChainIdError, useWeb3React } from '@web3-react/core';
+import { useEagerConnect } from './web3Hooks';
+import Typography from '@mui/material/Typography';
+import DialogContent from '@mui/material/DialogContent';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import ButtonBase from '@mui/material/ButtonBase';
 
 const style: SxProps<Theme> = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
   width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
   boxShadow: 24,
   p: 4,
 };
@@ -40,51 +33,49 @@ type Props = {
   setOpen: (open: boolean) => void;
 };
 
-const WalletButton = ({ wallet, handleConnect, imgSrc }) => {
-  return (
-    <ButtonBase
-      onClick={() => handleConnect(SUPPORTED_WALLETS[wallet].connector)}
-    >
-      {/* sx={{
+const WalletButton = ({ wallet, handleConnect, imgSrc }) => (
+  <ButtonBase
+    onClick={() => handleConnect(SUPPORTED_WALLETS[wallet].connector)}
+  >
+    {/* sx={{
                 "&:hover": {
                   bgcolor: "bgFocus.main",
                 },
               }} */}
-      <Paper
-        elevation={2}
+    <Paper
+      elevation={2}
+      sx={{
+        px: 3,
+        '&:hover': {
+          bgcolor: 'bgFocus.main',
+        },
+      }}
+    >
+      <Box
         sx={{
-          px: 3,
-          "&:hover": {
-            bgcolor: "bgFocus.main",
+          px: 5,
+          pb: 3,
+          pt: 7,
+        }}
+      >
+        <img alt="" src={imgSrc} width="100" height="100" />
+      </Box>
+      <Typography
+        color="primary.main"
+        sx={{
+          '&:hover': {
+            color: 'secondary.main',
           },
         }}
       >
-        <Box
-          sx={{
-            px: 5,
-            pb: 3,
-            pt: 7,
-          }}
-        >
-          <img src={imgSrc} width="100" height="100" />
-        </Box>
-        <Typography
-          color="primary.main"
-          sx={{
-            "&:hover": {
-              color: "secondary.main",
-            },
-          }}
-        >
-          {SUPPORTED_WALLETS[wallet].name}
-        </Typography>
-        <Typography color="textAdvanced.dark">
-          {SUPPORTED_WALLETS[wallet].description}
-        </Typography>
-      </Paper>
-    </ButtonBase>
-  );
-};
+        {SUPPORTED_WALLETS[wallet].name}
+      </Typography>
+      <Typography color="textAdvanced.dark">
+        {SUPPORTED_WALLETS[wallet].description}
+      </Typography>
+    </Paper>
+  </ButtonBase>
+);
 
 export default function WalletModal({ open, setOpen }: Props) {
   const handleClose = () => setOpen(false);
@@ -100,7 +91,7 @@ export default function WalletModal({ open, setOpen }: Props) {
   useEagerConnect();
 
   const handleConnect = async (connector: AbstractConnector | undefined) => {
-    let name = "";
+    let name = '';
     Object.keys(SUPPORTED_WALLETS).map((key) => {
       if (connector === SUPPORTED_WALLETS[key].connector) {
         return (name = SUPPORTED_WALLETS[key].name);
@@ -108,34 +99,33 @@ export default function WalletModal({ open, setOpen }: Props) {
       return true;
     });
 
-    console.log("connecting to the wallet", name);
+    console.log('connecting to the wallet', name);
 
     // if the connector is walletconnect and the user has already tried to connect, manually reset the connector
-    if (
-      connector instanceof WalletConnectConnector &&
-      connector.walletConnectProvider?.wc?.uri
-    ) {
+    if (connector instanceof WalletConnectConnector && connector.walletConnectProvider?.wc?.uri) {
       connector.walletConnectProvider = undefined;
     }
 
-    connector &&
+    if (connector) {
       activate(connector, undefined, true)
         .then(() => {
-          console.log("activate promise resolved");
+          console.log('activate promise resolved');
           setOpen(false);
         })
         .catch((error) => {
           if (error instanceof UnsupportedChainIdError) {
-            activate(connector); // a little janky...can't use setError because the connector isn't set
+            activate(connector); 
+            // a little janky...can't use setError because the connector isn't set
           } else {
-            console.log("connection error", error);
+            console.log('connection error', error);
             setError(error);
             // setPendingError(true)
           }
         });
+    }
 
     if (account) {
-      console.log("account connected");
+      console.log('account connected');
       setOpen(false);
     }
   };
@@ -150,7 +140,7 @@ export default function WalletModal({ open, setOpen }: Props) {
           <IconButton
             onClick={handleClose}
             sx={{
-              position: "absolute",
+              position: 'absolute',
               right: 8,
               top: 8,
               color: (theme: Theme) => theme.palette.textAdvanced.dark,
@@ -176,14 +166,13 @@ export default function WalletModal({ open, setOpen }: Props) {
               />
             </Grid>
           </Grid>
-          
-        
+
           <Typography
             component="p"
             color="textAdvanced.dark"
             align="center"
             variant="normal"
-            sx={{mt:3}}
+            sx={{ mt: 3 }}
           >
             By connecting a wallet, you agree to our
             <Typography
