7c387a1677e9fba0eb6d718d11a1d58d36a2c7ae
dileep2503 <98889160+dileep2503@users.noreply.github.com>
2022-02-25 08:55:56 +0530

    Update ScheduleEvent.tsx

--- snet-ui/Schedule/ScheduleEvent.tsx
+++ snet-ui/Schedule/ScheduleEvent.tsx
@@ -9,7 +9,7 @@ import { checkDateIsBetween, getDateInStandardFormat } from 'utils/date';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import moment from 'moment';
-import { Container, Divider } from '@mui/material';
+import { Container } from '@mui/material';
 
 type Event = {
   time: Date;
@@ -30,19 +30,19 @@ export default function ScheduleEvent({ event, nextEventTime }: ScheduleEventPro
   const formattedDate = getDateInStandardFormat(event.time);
   return (
     <Container>
-      <TimelineItem sx={{ bgcolor: 'textAdvanced.main', py: 3 }} key={event.id}>
+      <TimelineItem sx={{ bgcolor: 'textAdvanced.main' }} key={event.id}>
         <TimelineOppositeContent sx={{ display: 'none' }} />
         <TimelineSeparator>
           <TimelineDot sx={{ width: 19, height: 19, borderColor: 'common.white' }} color="primary" />
           {nextEventTime ? (
-            <TimelineConnector>
+            <TimelineConnector sx={{ height: 100 }}>
               {isActiveEvent ? (
                 <Typography
                   sx={{
                     position: 'absolute',
                     bgcolor: 'bgHighlight.main',
                     color: 'textAdvanced.dark',
-                    mt: 3,
+                    mt: 7,
                   }}
                   variant="body2"
                 >
@@ -56,14 +56,26 @@ export default function ScheduleEvent({ event, nextEventTime }: ScheduleEventPro
         <TimelineContent>
           <Grid container spacing={4}>
             <Grid item xs={4}>
-              <Typography variant="h6" fontSize="18px" color="primary">
-                {formattedDate}
-              </Typography>
+              {isActiveEvent ? (
+                <Typography variant="h6" fontSize="18px" color="green" pb="10px">
+                  {formattedDate}
+                </Typography>
+              ) : (
+                <Typography variant="h6" fontSize="18px" color="primary">
+                  {formattedDate}
+                </Typography>
+              )}
             </Grid>
             <Grid item xs={8}>
-              <Typography variant="h6" fontSize="18px" color="primary" component="p">
-                {event.title}
-              </Typography>
+              {isActiveEvent ? (
+                <Typography variant="h6" fontSize="18px" color="green">
+                  {event.title}
+                </Typography>
+              ) : (
+                <Typography variant="h6" fontSize="18px" color="primary" component="p">
+                  {event.title}
+                </Typography>
+              )}
               <Typography variant="normal" fontSize="14px" color="textAdvanced.primary">
                 {event.description}
               </Typography>
